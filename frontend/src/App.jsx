a64abff1acffc9754b1fec334c8f7f80201b0f41
tanner-schmutte <tannerschmutte@gmail.com>
2024-09-25 20:40:18 -0500

    split Auth into its own component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,67 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { FaGear } from "react-icons/fa6";
-import "./App.css";
+import Auth from "./Auth/auth";
 
 function App() {
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        fetch(`${import.meta.env.VITE_BACKEND_URL}/user`, {
-            credentials: "include",
-        })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error("Not logged in");
-            })
-            .then((data) => {
-                setUser(data);
-            })
-            .catch((error) => console.log(error));
-    }, []);
-
-    const handleLogin = () => {
-        window.location.href = `${
-            import.meta.env.VITE_BACKEND_URL
-        }/auth/procore`;
-    };
-
-    const handleLogout = () => {
-        fetch(`${import.meta.env.VITE_BACKEND_URL}/logout`, {
-            method: "POST",
-            credentials: "include",
-        }).then(setUser(null));
-    };
-
-    return (
-        <>
-            <div className="title">
-                <FaGear
-                    style={{
-                        color: "white",
-                        fontSize: "36px",
-                        paddingRight: "10px",
-                    }}
-                />
-                ERP App
-            </div>
-            <div className="button-container">
-                {user ? (
-                    <>
-                        <div className="welcome">Welcome, {user.login}</div>
-                        <button className="auth-button" onClick={handleLogout}>
-                            Logout
-                        </button>
-                    </>
-                ) : (
-                    <button className="auth-button" onClick={handleLogin}>
-                        Login with Procore
-                    </button>
-                )}
-            </div>
-        </>
-    );
+    return <Auth />;
 }
 
 export default App;
