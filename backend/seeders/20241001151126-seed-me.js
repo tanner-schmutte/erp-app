482a0af84ece2f82c7d4d1bb5018685e9126f2ab
tanner-schmutte <tannerschmutte@gmail.com>
2024-10-01 10:21:47 -0500

    seed myself

---
+++ backend/seeders/20241001151126-seed-me.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.bulkInsert(
+            "Users",
+            [
+                {
+                    email: "tanner.schmutte@procore.com",
+                    role: "admin",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            ],
+            {}
+        );
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.bulkDelete("Users", null, {});
+    },
+};
