8fa7e6d4aae39421a13346f6903543dee4283f91
tanner-schmutte <tannerschmutte@gmail.com>
2024-11-12 11:22:27 -0600

    add production

--- backend/config/config.js
+++ backend/config/config.js
@@ -8,4 +8,14 @@ module.exports = {
         host: process.env.DB_HOST,
         dialect: "postgres",
     },
+    production: {
+        use_env_variable: "DATABASE_URL",
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false, // This ensures the SSL certificate is accepted
+            },
+        },
+    },
 };
